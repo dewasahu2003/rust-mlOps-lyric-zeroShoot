ae57658231d1e9d07dfd3b09b386776de6076d31
dewa <95997298+dewasahu2003@users.noreply.github.com>
2023-08-06 10:57:39 +0000

    simple changes to main.rs

--- lyric/src/main.rs
+++ lyric/src/main.rs
@@ -1,3 +1,13 @@
 use clap::Parser;
 
 #[derive(Parser)]
+#[clap(
+    version="1.0",
+    author="Dewa Sahu",
+    about = "A Command-line tool to analyze lyrics to songs and put them into a sqlite database."
+)]
+
+struct Cli{
+    #[clap(subcommand)]
+    command:Option<Commands>,
+}
