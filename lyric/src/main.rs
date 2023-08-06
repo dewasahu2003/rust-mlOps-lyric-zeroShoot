c5744c5cf0da08a991dbdddd153583f6a7d61fff
dewa <95997298+dewasahu2003@users.noreply.github.com>
2023-08-06 11:15:13 +0000

    changes to main

--- lyric/src/main.rs
+++ lyric/src/main.rs
@@ -2,12 +2,31 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[clap(
-    version="1.0",
-    author="Dewa Sahu",
+    version = "1.0",
+    author = "Dewa Sahu",
     about = "A Command-line tool to analyze lyrics to songs and put them into a sqlite database."
 )]
 
-struct Cli{
+struct Cli {
     #[clap(subcommand)]
-    command:Option<Commands>,
+    command: Option<Commands>,
+}
+
+#[derive(Parser)]
+enum Commands {
+    #[clap(version = "1.0", author = "Dewa sahu")]
+    Classify {
+        #[clap(short, long, default_value = "lyrics.txt")]
+        file: String,
+    },
+    Candidates {},
+    Lyrics {
+        #[clap(short, long, default_value = "lyrics.txt")]
+        file: String,
+    },
+}
+
+fn main(){
+    let args=Cli::parse()
+    
 }
